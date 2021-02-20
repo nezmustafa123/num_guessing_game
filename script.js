efdf122b85b9c2057ef6de3e8d7e2f9e2ab94e3d
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-20 16:36:36 +0000

    initial dom manipulation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//dom structured representation of html documents
+
+//whatever is in the html document is in the dom
+
+//dom part of web api
+
+//query selector method to select a method pass in selector into method same ass css
+//multiple dom operators executed from left to right
+//get text of element
+console.log(document.querySelector('.message').textContent);
