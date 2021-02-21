674edc92f90ea693170fdb98c8db772950f04729
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-21 19:57:11 +0000

    more DOM manipulation changing the text content and input value

--- script.js
+++ script.js
@@ -10,3 +10,16 @@
 //multiple dom operators executed from left to right
 //get text of element
 console.log(document.querySelector('.message').textContent);
+
+//change text content to something else set it to a string
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+//get data from input element and set it
+
+
+document.querySelector('.guess').value = 15
+console.log(document.querySelector('.guess').value);
+
