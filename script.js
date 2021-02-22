aaa34cbf106f5a670b7cae881524c2a3c3c19fe2
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-22 16:57:45 +0000

    adding event handlers

--- script.js
+++ script.js
@@ -9,17 +9,22 @@
 //query selector method to select a method pass in selector into method same ass css
 //multiple dom operators executed from left to right
 //get text of element
-console.log(document.querySelector('.message').textContent);
+//console.log(document.querySelector('.message').textContent);
+//
+////change text content to something else set it to a string
+//document.querySelector('.message').textContent = 'Correct Number!';
+//
+//document.querySelector('.number').textContent = 13;
+//document.querySelector('.score').textContent = 20;
+//
+////get data from input element and set it
+//
+//
+//document.querySelector('.guess').value = 15
+//console.log(document.querySelector('.guess').value);
+//
 
-//change text content to something else set it to a string
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-//get data from input element and set it
-
-
-document.querySelector('.guess').value = 15
-console.log(document.querySelector('.guess').value);
-
+//event handlers function is just a value so pass it onto function as an argument
+document.querySelector('.check').addEventListener('click', function(){
+    console.log(document.querySelwctor('.guess').value);
+});
